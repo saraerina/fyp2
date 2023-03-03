36b308fa26ffc2f049a8e42cc93938514df578b2
Sara Erina <saraerinayap@gmail.com>
2023-03-03 17:54:17 +0800

    add space

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,7 +64,7 @@ class RegisterController extends Controller
      */
     public function create(array $data)
     {
-        dd($data);
+        dd($data); 
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
